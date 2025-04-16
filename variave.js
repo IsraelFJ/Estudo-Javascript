99ad24fd1f4e68c37ab29754093667672d4479c3
IsraelFJ <israel.jesus@ba.estudante.senai.br>
2025-04-16 19:25:40 -0300

    Primeiro commit

---
+++ variave.js
@@ -0,0 +1,21 @@
+// Variaveis de ecopo local mais moderna que a var, sendo o ideasl utilisar sempre let 
+let nome = 'jeca tatu'
+
+// Criando em var é uma variavel de escopo global var como exemplo 
+var name = 'ariano suassuna'
+
+// Criando uima constante 
+const idade = 30
+
+// Utilizado para exibir um teste de logica na tela 
+console.log('Idade:', idade)
+
+// Template String 
+console.log(`Idade: ${idade}`)
+
+// Condiciaonais em javaScritp
+if (idade < 18){
+    console.log('Menor de idade')
+} else{
+    console.log('Maior de idade')
+}
